71ace6b53efce994a1cdded8dd464f31702bf720
Ilia Jamasb <ijamasb03@gmail.com>
2025-09-29 11:30:01 +0100

    EDIT: users can guess full word

--- hangman.py
+++ hangman.py
@@ -22,20 +22,31 @@ print(chosenWord)
 print(guessletters)
 
 while livesCounter > 0:
-    userinput = input("Enter your letter \n")
-    if len(userinput) !=1:
-        print("Your input must be one letter!")
-        continue
-    userinput = userinput.lower()
-    for i in range(len(chosenWord)):
-        if userinput == chosenWord[i]:
-            guessletters[i] = userinput
-    if userinput not in chosenWord:
-        livesCounter -= 1
-    print(guessletters)
     print("You have " + str(livesCounter) + " lives left")
-    if "_" not in guessletters:
-        print("You win!")
-        break
+    userinput = input("Enter your letter \n")
+    userinput = userinput.lower()
+    if len(userinput) == len(chosenWord):
+        if(userinput == chosenWord):
+            print("You win!")
+            break
+        else:
+            print("Incorrect guess!")
+            livesCounter-=1
+    elif len(userinput) !=1:
+        print("Your input must be one letter, or the exact length of the word!")
+        continue
+    else:
+        for i in range(len(chosenWord)):
+            if userinput == chosenWord[i]:
+                guessletters[i] = userinput
+        if userinput not in chosenWord:
+            livesCounter -= 1
+        print(guessletters)
+        if "_" not in guessletters:
+            print("You win!")
+            break
+    print()
 if livesCounter == 0:
     print("You lost! Haha, the word was " + chosenWord)
+
+
