f410df8c3d6b5db990bd25873f74878c98796fd0
Abdul Bari <barii02@hotmail.com>
2025-09-29 10:13:00 +0100

    Added a loop functionality

--- rps.py
+++ rps.py
@@ -3,27 +3,35 @@ import random
 playerWinCount = 0
 computerWinCount = 0
 
+
 print("Welcome top rock paper scissors game!")
-playerInput = input("It's your turn! r/p/s \n")
-comChoices = {0: "r", 1: "p", 2: "s"}
-comTurn = comChoices[random.randint(0,2)]
 
-print("The computer went: " + comTurn)
+while playerWinCount < 2 and computerWinCount < 2:
+    playerInput = input("It's your turn! r/p/s \n")
+    comChoices = {0: "r", 1: "p", 2: "s"}
+    comTurn = comChoices[random.randint(0,2)]
 
-round = playerInput+comTurn
-playerResults = {
-    "rr": "d", "rp": "l", "rs": "w",
-    "pp": "d", "pr": "w", "ps": "l",
-    "ss": "d", "sr": "l", "sp": "w"
-    }
+    print("The computer went: " + comTurn)
 
-playerOutcome = playerResults[round]
+    round = playerInput+comTurn
+    playerResults = {
+        "rr": "d", "rp": "l", "rs": "w",
+        "pp": "d", "pr": "w", "ps": "l",
+        "ss": "d", "sr": "l", "sp": "w"
+        }
 
-if playerOutcome == "w":
-    print("You win!")
-    playerWinCount+=1
-elif playerOutcome == "d":
-    print("It's a draw!")
+    playerOutcome = playerResults[round]
+
+    if playerOutcome == "w":
+        print("You won this round!")
+        playerWinCount+=1
+    elif playerOutcome == "d":
+        print("It's a draw!")
+    else:
+        print("You lost this round!")
+        computerWinCount+=1
+
+if playerWinCount == 2:
+    print("You Won!!!!!")
 else:
-    print("You lose!")
-    computerWinCount+=1
+    print("Computer Won!")
